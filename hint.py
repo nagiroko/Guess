f217aab573511b11edb8d3ecb973276343bfceb6
nagiroko <hoobrajrohit@yahoo.com>
2024-01-22 15:25:08 -0500

    hint v2
    revamped the hint system by having a single function to ask what hint the player wants then it calls a corresponding function to get the answer

--- hint.py
+++ hint.py
@@ -1,7 +1,25 @@
 from Card import card
 
 def answer(hidden):
-    if hidden.value % 2 != 0:
-        print("card is odd")
+    print("Enter 1 if you want to know if the card is odd or even")
+    answered = False
+
+    while answered == False:
+        try:
+            question = input()
+            question = int(question)
+            match question:
+                case 1:
+                    oddoreven(hidden)
+                    answered = True
+                case _:
+                    print("number was not associated with a question please try again")
+        except:
+            print("answer was not a whole number please try again")
+
+
+def oddoreven(var):
+    if var.value % 2 != 0:
+        print("your card is odd")
     else:
-        print("card is even")
+        print("your card is even")
